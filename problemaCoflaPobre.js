5ec521ecb1015a5d3f2274be6bea602613d791c2
D4SH3R <miguelpaulino239@gmail.com>
2022-02-03 17:29:28 -0500

    Funciona bien

---
+++ problemaCoflaPobre.js
@@ -0,0 +1,23 @@
+let free = false;
+
+const validarEntrada = (time)=>{
+    let edad = prompt("Que edad tienes?");
+    if (edad >= 18){
+        if(time >= 2 && time <= 7 && free == false){
+            alert(`Son las ${time}:00hr. Puedes pasar gratis eres el primero`);
+            free = true;
+        }else{
+            alert(`Son las ${time}:00hr. Tienes que pagar, ya entro el primero!`);
+        }
+    }else {
+        alert("Eres menor de edad chamaco. No puedes entrar!");
+    }
+}
+
+validarEntrada(22);
+validarEntrada(23);
+validarEntrada(1);
+validarEntrada(1.2);
+validarEntrada(2);
+validarEntrada(3);
+validarEntrada(10);
