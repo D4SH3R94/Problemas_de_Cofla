15f1083626715516b05379e74eff9ba8ff75bd3c
D4SH3R <miguelpaulino239@gmail.com>
2022-02-04 11:55:19 -0500

    Esta funcional y correcto

--- problemaCoflaPobre.js
+++ problemaCoflaPobre.js
@@ -2,12 +2,13 @@ let free = false;
 
 const validarEntrada = (time)=>{
     let edad = prompt("Que edad tienes?");
+    console.log(time)
     if (edad >= 18){
         if(time >= 2 && time <= 7 && free == false){
             alert(`Son las ${time}:00hr. Puedes pasar gratis eres el primero`);
             free = true;
-        }else if(time > 7){
-            alert("Ya cerro la fiesta");
+        }else if(time < 2 || time > 7){
+            alert("Esta cerrado la nueva fiesta empieza a las 2:00 am.");
         }else{
             alert(`Son las ${time}:00hr. Tienes que pagar, ya entro el primero!`);
         }
