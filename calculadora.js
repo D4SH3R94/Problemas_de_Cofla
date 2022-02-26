9bd5ca6e659a7508a0374ca39fe3d4ea7797d645
miguelpaulino239@gmail.com <miguelpaulino239@gmail.com>
2022-02-26 16:20:00 -0500

    Mejore la calc

--- calculadora.js
+++ calculadora.js
@@ -10,9 +10,23 @@ const dividir = (num1,num2)=>{
 const multiplicar = (num1,num2)=>{
     return parseInt(num1) * parseInt(num2);
 }
+const potencia =(num1,exp)=>{
+    let resultado = num1;
+    for (let i = 1; i < exp; i++) {
+        resultado = resultado * num1;
+        }return resultado;
+}
+const raizCuadrada= (num1)=>{
+    num1 = Math.sqrt(num1);
+    return num1;
+}
+const raizCubica = (num1)=>{
+    num1 = Math.cbrt(num1);
+    return num1;
+}
 
 alert("Que calculo quieres hacer?");
-let calculo = prompt("1- para sumar, 2- para restar, 3- para dividir, 4- para multiplicar");
+let calculo = prompt("1- para sumar, 2- para restar, 3- para dividir, 4- para multiplicar, 5- para raiz cuadrada, 6- para raiz cubica, 7- para potenciacion");
 
 const calculadora = ()=>{
 
@@ -39,6 +53,22 @@ if (calculo == 1){
     valor1 = prompt("El primer numero es?");
     valor2 = prompt("EL segundo numero es?");
     let resultado = multiplicar(valor1,valor2);
+    document.write(resultado);
+}else if(calculo == 5){
+    alert("De que numero quieres encontrar la raiz");
+    valor1 = prompt("Diga el numero");
+    let resultado = raizCuadrada(valor1);
+    document.write(resultado);
+}else if(calculo == 6){
+    alert("De que numero quieres encontrar la raiz cubica");
+    valor1 = prompt("Diga el numero");
+    let resultado = raizCubica(valor1);
+    document.write(resultado);
+}else if(calculo == 7){
+    alert("Diga el numero y el exponente");
+    valor1 = prompt("Diga el numero base");
+    valor2 = prompt("Diga el numero del exponente")
+    let resultado = potencia(valor1,valor2);
     document.write(resultado);
 }else{
     alert("Por favor haga un calculo correcto");
